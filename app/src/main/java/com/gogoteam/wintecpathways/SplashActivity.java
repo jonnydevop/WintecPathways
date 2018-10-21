984c34cc662855dc881e01276c8bb7936cc09e28
juancabelloo <42507624+juancabelloo@users.noreply.github.com>
2018-10-21 19:18:50 +1300

    Initial commit

---
+++ app/src/main/java/com/gogoteam/wintecpathways/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.gogoteam.wintecpathways;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo=(ImageView)findViewById(R.id.logo);
+
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
+    }
+}
