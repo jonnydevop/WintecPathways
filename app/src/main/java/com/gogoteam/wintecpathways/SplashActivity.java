422124b37c1af33653c6c7ca1f65587456b2f289
juancabelloo <42507624+juancabelloo@users.noreply.github.com>
2018-10-21 21:19:14 +1300

    Disclamer Activity

--- app/src/main/java/com/gogoteam/wintecpathways/SplashActivity.java
+++ app/src/main/java/com/gogoteam/wintecpathways/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.gogoteam.wintecpathways;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.animation.Animation;
@@ -9,6 +11,7 @@ import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private int SPLASH_TIME_OUT=3000;
     private ImageView logo;
 
     @Override
@@ -17,7 +20,19 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         logo=(ImageView)findViewById(R.id.logo);
 
+        //Activate animation
         Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mysplashanimation);
         logo.startAnimation(myanim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
