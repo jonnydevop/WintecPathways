a48a1b4cdce155d83ee70feb4db0d15a6c02b76b
juancabelloo <42507624+juancabelloo@users.noreply.github.com>
2018-10-23 15:13:34 +1300

    Pathway Activity created

---
+++ app/src/main/java/com/gogoteam/wintecpathways/PathwayActivity.java
@@ -0,0 +1,42 @@
+package com.gogoteam.wintecpathways;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PathwayActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pathway);
+
+        ActionBar actionBar = getSupportActionBar();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem.OnActionExpandListener onActionExpandListener = new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem menuItem) {
+                setContentView(R.layout.search_activity);
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem menuItem) {
+                setContentView(R.layout.activity_pathway);
+                return true;
+            }
+        };
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        searchItem.setOnActionExpandListener(onActionExpandListener);
+        return true;
+    }
+}
