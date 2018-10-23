017c320badaa5d79fb76eb5a51336cf180384758
juancabelloo <42507624+juancabelloo@users.noreply.github.com>
2018-10-23 13:17:48 +1300

    Pathway Activity created

--- app/src/main/java/com/gogoteam/wintecpathways/MainActivity.java
+++ app/src/main/java/com/gogoteam/wintecpathways/MainActivity.java
@@ -2,12 +2,17 @@ package com.gogoteam.wintecpathways;
 
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    CardView studentCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
             disc.show();
             disc.getWindow().setBackgroundDrawableResource(R.color.endOrangeGradient);
         }
+
+        studentCard = (CardView)findViewById(R.id.studentView);
+        studentCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, PathwayActivity.class));
+            }
+        });
     }
 
 }
