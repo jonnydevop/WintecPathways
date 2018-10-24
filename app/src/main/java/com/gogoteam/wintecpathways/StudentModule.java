1eb4df480a9aff2314e9c8fe3280e48621116c55
linzha93 <linzha93@student.wintec.ac.nz>
2018-10-24 15:45:32 +1300

    Add class: Module & StudentModule

---
+++ app/src/main/java/com/gogoteam/wintecpathways/StudentModule.java
@@ -0,0 +1,44 @@
+package com.gogoteam.wintecpathways;
+
+public class StudentModule {
+    private	String SMID;
+    private	String SID;
+    private	String MID;
+    private	String Completion;
+
+    public StudentModule(String SID, String MID, String Completion) {
+        this.SID = SID;
+        this.MID = MID;
+        this.Completion = Completion;
+    }
+
+    public String getSMID() {
+        return SMID;
+    }
+
+    public void setSMID(String SMID) {
+        this.SMID = SMID;
+    }
+
+    public String getSID() {
+        return SID;
+    }
+
+    public void setSID(String SID) {
+        this.SID = SID;
+    }
+
+    public String getMID() {
+        return MID;
+    }
+
+    public void setMID(String MID) { this.MID = MID; }
+
+    public String getCompletion() {
+        return Completion;
+    }
+
+    public void setCompletion(String completion) {
+        Completion = completion;
+    }
+}
