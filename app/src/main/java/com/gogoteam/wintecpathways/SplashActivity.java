f01622719190104bb9017c8988e8fac3247adac7
venkatvenkatr <44336487+venkatvenkatr@users.noreply.github.com>
2018-10-25 16:49:28 +1300

    splash update

--- app/src/main/java/com/gogoteam/wintecpathways/SplashActivity.java
+++ app/src/main/java/com/gogoteam/wintecpathways/SplashActivity.java
@@ -1,17 +1,19 @@
 package com.gogoteam.wintecpathways;
 
 import android.content.Intent;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private int SPLASH_TIME_OUT=3000;
+   /** private int SPLASH_TIME_OUT=3000;
     private ImageView logo;
 
     @Override
@@ -35,4 +37,48 @@ public class SplashActivity extends AppCompatActivity {
         }, SPLASH_TIME_OUT);
 
     }
+} */
+   @Override
+   public void onAttachedToWindow() {
+       super.onAttachedToWindow();
+       Window window = getWindow();
+       window.setFormat(PixelFormat.RGBA_8888);
+   }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView = (ImageView)findViewById(R.id.logo);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.mysplashanimation);
+        imageView.setAnimation(animation);
+
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    int waited = 0;
+                    while(waited < 1000){
+                        sleep(400);
+                        waited +=100;
+                    }
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                    startActivity(intent);
+                    SplashActivity.this.finish();
+
+                }catch (InterruptedException e){
+
+                }finally {
+                    SplashActivity.this.finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
 }
+
+
